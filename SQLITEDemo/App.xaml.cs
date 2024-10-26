197c995fa579a18496733ce5e8dd69b3abdc7a5e
Donghwi_Park <gptnc5100@gmail.com>
2024-10-26 20:13:52 +0200

    Add project files.

---
+++ SQLITEDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SQLITEDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
