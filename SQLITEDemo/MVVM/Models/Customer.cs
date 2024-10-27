cba068e92ae80ce8100ace227636c335e80958d6
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-10-27 20:12:12 +0100

    Creating the customer model

---
+++ SQLITEDemo/MVVM/Models/Customer.cs
@@ -0,0 +1,9 @@
+namespace SQLITEDemo.MVVM.Models;
+
+public class Customer
+{
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+}
