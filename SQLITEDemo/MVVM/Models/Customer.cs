0be0cdbb2b5b57ff1b87ff74d6c95cedcb63f414
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-10-27 20:20:58 +0100

    Applying SQLite attributes to a C# class.

--- SQLITEDemo/MVVM/Models/Customer.cs
+++ SQLITEDemo/MVVM/Models/Customer.cs
@@ -1,9 +1,22 @@
+using SQLite;
+
 namespace SQLITEDemo.MVVM.Models;
 
+[System.ComponentModel.DataAnnotations.Schema.Table("Customers")]
 public class Customer
 {
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+    
+    [System.ComponentModel.DataAnnotations.Schema.Column("name"), Indexed, NotNull]
     public string Name { get; set; }
+    [Unique]
     public string Phone { get; set; }
     public int Age { get; set; }
+    [MaxLength(100)]
     public string Address { get; set; }
+    
+    [Ignore] 
+    public bool IsYoung =>
+        Age > 50 ? true : false;
 }
