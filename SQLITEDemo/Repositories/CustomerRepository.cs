fca0fb93bf5d4a8918f17dae0cb127d83742281b
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-10-27 20:44:17 +0100

    Inserting a customer

--- SQLITEDemo/Repositories/CustomerRepository.cs
+++ SQLITEDemo/Repositories/CustomerRepository.cs
@@ -6,10 +6,25 @@ namespace SQLITEDemo.Repositories;
 public class CustomerRepository
 {
     SQLiteConnection connection { get; set; }
+    public string StatusMessage { get; set; }
 
     public CustomerRepository()
     {
         connection = new SQLiteConnection(Constants.DatabasePath, Constants.Flags);
         connection.CreateTable<Customer>();
+    }
+
+    public void Add(Customer newCustomer)
+    {
+        int result = 0;
+        try
+        {
+            result = connection.Insert(newCustomer);
+            StatusMessage = $"{result} row(s) created"; 
+        }
+        catch (Exception e)
+        {
+            StatusMessage = $"Error : {e.Message}";
+        }
     }
 }
